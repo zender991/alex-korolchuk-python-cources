4e04351dbfb3d0f8ea7e3473025bd4f5169250b2
zender991 <zender991@gmail.com>
2017-10-21 17:57:18 +0300

    fix for seconnd task

--- HT_1/2.py
+++ HT_1/2.py
@@ -4,10 +4,24 @@ which are not present in color_list_2. '''
 color_list_1 = set(["White", "Black", "Red"])
 color_list_2 = set(["Red", "Green"])
 
+#------------first implementation---------------
+
+difference_set_1 = color_list_1 - color_list_2  #minus makes difference between sets
+print(difference_set_1)
+
+#------------second implementation---------------
+
+difference_set_2 = color_list_1.difference(color_list_2) #method difference makes difference between sets
+print(difference_set_2)
+
+#------------third implementation---------------
+
 color_list_3 = [] #initiate empty list
 
 for color in color_list_1:  #select each color from the list 1
     if color not in color_list_2: #check selected color in the list 2
         color_list_3.append(color)  #add color that doesn't present in the list 2
 
-print(color_list_3) #print new list with needed colors
+difference_set_3 = set(color_list_3)
+print(difference_set_3) #print new list with needed colors
+
