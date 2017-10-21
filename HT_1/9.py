27db3694ee5a4f07018f235da2c7fddcaa8808eb
zender991 <zender991@gmail.com>
2017-10-21 19:09:51 +0300

    fix for 9th task

--- HT_1/9.py
+++ HT_1/9.py
@@ -1,12 +1,10 @@
-'''9. Write a script to remove an empty tuples from a list of tuples'''
-
 initial_list = [(), (), ('',), ('a', 'b'), (), ('a', 'b', 'c'), ('d')]
+output_list = []
 
-j = 0
 
-for i in initial_list:              #select each tuple in a list
-    if len(i) == 0:                 #chech if tiple is empty
-        del initial_list[j]         #if empty, remove tuple from a list by index
-    j = j + 1                       #increase conter for indexes
+for i in initial_list:    #select each tuple in a list
+    if len(i) > 0:
+        output_list.append(i)   #if tuple isn't empty - add to new list
 
-print(initial_list)
+
+print(output_list)
