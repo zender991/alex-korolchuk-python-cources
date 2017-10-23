602daea3e8ef753deb2a9e885d45902de79f33c7
zender991 <zender991@gmail.com>
2017-10-23 23:07:46 +0300

    add season3 func for task 1

--- HT_2/ht_2.py
+++ HT_2/ht_2.py
@@ -1,10 +1,10 @@
-'''Написати функцію season, приймаючу 1 аргумент — номер місяця (від 1 до 12),
+''' Task 1. Написати функцію season, приймаючу 1 аргумент — номер місяця (від 1 до 12),
 яка буде повертати пору року, якій цей місяць належить (зима, весна, літо або осінь).'''
 
 
 def season1(number):
     return {1:'winter', 2:'winter', 3:'spring', 4:'spring', 5:'spring', 6:'summer', 7:'summer', 8:'summer',
-            9:'autumn', 10:'autumn', 11:'autumn', 12:'winter'}.get(number)
+            9:'autumn', 10:'autumn', 11:'autumn', 12:'winter'}.get(number, 'Invalid number')
 
 # example - season1(7)
 
@@ -15,14 +15,26 @@ def season2(number):
         return 'summer'
     elif number >= 9 and number <= 11:
         return 'autumn'
-    else:
+    elif number >= 1 and number <= 2 or number == 12:
         return 'winter'
+    else:
+        return 'Invalid number'
 
 # example - season2(7)
 
+def season3(number):
+    seasones_list = ['winter', 'spring', 'spring', 'spring', 'summer', 'summer', 'summer',
+                     'autumn', 'autumn', 'autumn', 'winter', 'winter']
 
+    if number >= 1 and number <= 12:
+        return seasones_list[number - 1]
+    else:
+        return "Invalid number"
+
+# example - season3(7)
 
 entered_value = int(input('Enter month number - '))
 
-print('First function : ' + str(season1(entered_value)))
-print('Second function : ' + str(season2(entered_value)))
+#print('First function : ' + str(season1(entered_value)))
+#print('Second function : ' + str(season2(entered_value)))
+#print('Third function : ' + str(season3(entered_value)))
