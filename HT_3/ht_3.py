d26d54d5285e4510144a9ef4133cd6635eaf4d1e
zender991 <zender991@gmail.com>
2017-11-02 21:08:02 +0200

    add print_result func

--- HT_3/ht_3.py
+++ HT_3/ht_3.py
@@ -28,19 +28,20 @@ def bubble_sort(list_for_bubble):
 
 
 
-def insertion_sort(list):
-    for i in range(len(list) - 1):
+def insertion_sort(list_for_ins):
 
-        curr_value = list[i]
+    for i in range(1, len(list_for_ins)):
+
+        curr_value = list_for_ins[i]
         curr_index = i
 
-        while curr_index > 0 and list[curr_index - 1] > curr_value:
-            list[curr_index] = list[curr_index - 1]
+        while curr_index > 0 and list_for_ins[curr_index - 1] > curr_value:
+            list_for_ins[curr_index] = list_for_ins[curr_index - 1]
             curr_index = curr_index - 1
 
-    list[curr_index] = curr_value
+        list_for_ins[curr_index] = curr_value
 
-    return list
+    return list_for_ins
 
 
 def calculate_time(sort_func, sort_name):
@@ -64,29 +65,22 @@ def compare_lists(first_list, second_list):
 
 
 
-native_sorted_list = native_sort(test_list)   # get int sorted list with native sort function
-native_sorted_list_float = native_sort(test_list_float)   # get float sorted list with native sort function
+def print_result (test_list, sort_type ):
 
 
-sorting_time_bubble_int, sorted_list_bubble_int = calculate_time(bubble_sort, test_list)   # get values for int bubble sorting
-sorting_time_bubble_float, sorted_list_bubble_float = calculate_time(bubble_sort, test_list_float)   # get values for int bubble sorting
+    native_sorted_list = native_sort(test_list)
+    sorting_time, sorted_list = calculate_time(sort_type, test_list)
 
-sorting_time_insertion_int, sorted_list_insertion_int = calculate_time(insertion_sort, test_list)   # get values for int bubble sorting
-
-
-
-print('------ Sorting for int list ------')
-
-#print('bubble sort: time = ' + str("{0:.4f}".format(sorting_time_bubble_int)) +            # display int bubble algorithm time, used .format for 4 digits after point,
-#      ', The list is sorted correctly - ' + str(compare_lists(sorted_list_bubble_int, native_sorted_list)))      #return True if native and custom sorting results are equal
-
-print('insertion sort: time = ' + str("{0:.4f}".format(sorting_time_insertion_int)) +            # display int bubble algorithm time, used .format for 4 digits after point,
-      ', The list is sorted correctly - ' + str(compare_lists(sorted_list_insertion_int, native_sorted_list)))      #return True if native and custom sorting results are equal
+    return print(str(sort_type) + ' : time = ' + str("{0:.4f}".format(
+        sorting_time)) +  # display int bubble algorithm time, used .format for 4 digits after point,
+          ', The list is sorted correctly - ' + str(compare_lists(sorted_list,
+                                                                  native_sorted_list)))  # return True if native and custom sorting results are equal
 
 
 
+#print_result(test_list, bubble_sort)
+#print_result(test_list, insertion_sort)
+#print_result(test_list_float, bubble_sort)
+#print_result(test_list_float, insertion_sort)
 
-print('------ Sorting for float list ------')
-#print('bubble sort: time = ' + str("{0:.4f}".format(sorting_time_bubble_float)) +            # display float bubble algorithm time, used .format for 4 digits after point,
-#      ', The list is sorted correctly - ' + str(compare_lists(sorted_list_bubble_float, native_sorted_list_float)))      #return True if native and custom sorting results are equal
 
