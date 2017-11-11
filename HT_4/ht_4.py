efd3b5a12f2111f6382b9b098bee601792d4570d
zender991 <zender991@gmail.com>
2017-11-11 19:03:30 +0200

    Add unique values

--- HT_4/ht_4.py
+++ HT_4/ht_4.py
@@ -3,15 +3,10 @@ import csv
 
 
 file = open('test.log')
-
 filetext = file.read()
-
-#test_string = "2017-11-10 20:02:57,062 1176 ERROR pg openerp.addons.fetchmail.fetchmail: General failure when trying to fetch mail from pop server info@some_product.de."
-
 
 result = re.findall(r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}),\d{3} \d{4,5} (ERROR|WARNING|CRITICAL) \D{1,4} (.+)', filetext)
 
-print(result)
 
 with open('filename.csv', 'w') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
@@ -22,3 +17,35 @@ with open('filename.csv', 'w') as myfile:
         i = [line_id, i[1], i[0], i[2]]
         wr.writerow(i)
         line_id += 1
+
+counter = 0
+
+unique_list = set(result)
+unique_list1 = []
+c_co = 0
+
+for i in unique_list:
+    for j in result:
+        co = j.count(i[2])
+        c_co += co
+        #print(c_co)
+
+    #unique_list1.append([list(i]])
+    k = list(i)
+    k.append(c_co)
+    unique_list1.append(k)
+    c_co = 0
+
+
+with open('unique.csv', 'w') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    t = ['count','marker', 'data_time', 'description']
+    wr.writerow(t)
+    for i in unique_list1:
+        i = [i[3], i[1], i[0], i[2]]
+        wr.writerow(i)
+
+
+print(unique_list)
+print(unique_list1)
+
