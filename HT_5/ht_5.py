380733d909a12cda92b47dbf25d91630932f9b43
zender991 <zender991@gmail.com>
2017-11-17 00:38:48 +0200

    add filters

--- HT_5/ht_5.py
+++ HT_5/ht_5.py
@@ -1,44 +1,44 @@
 import json
 import requests
 import argparse
+import csv
 import logging
 import logging.config
+import time
+import datetime
 from config import category_list, default_category
 
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger("exampleApp")
 
-
-
-#logging.basicConfig(filename="sample.log", level=logging.INFO)
 logger.info("Programm started")
 
 arg_parser = argparse.ArgumentParser(description='Parser for categories')
 arg_parser.add_argument("--cat","-c", type=str, choices=category_list, default=default_category, help="Category name")
+arg_parser.add_argument("--score","-s", type=int, default=0, help="Score number")
+arg_parser.add_argument("--date","-d", type=str, default="01/01/2000", help="Date in d/m/y format")
 options = arg_parser.parse_args()
 logger.info("Received category name - %s"%options.cat)
-print(options.cat)
 
-
-#category = "newstories"
 category = options.cat
+score = options.score
+date = time.mktime(datetime.datetime.strptime(options.date, "%d/%m/%Y").timetuple())
 
-category_url = ("https://hacker-news.firebaseio.com/v0/%s.json?print=pretty"%category)
-response = requests.get(category_url, timeout=5)
-data = response.json()
+try:
+    category_url = ("https://hacker-news.firebaseio.com/v0/%s.json?print=pretty"%category)
+    response = requests.get(category_url, timeout=5)
+    data = response.json()
+except Exception as e:
+    logger.error(e)
 
-
-
-#
 # response = urllib.urlopen(url, timeout=5)
 # data = json.loads(response.read())
 
-
-print(data)
+#print(data)
 
 json_list = []
 
-for i in data:
+for i in data[:4]:
     url_for_items = ("https://hacker-news.firebaseio.com/v0/item/%i.json?print=pretty" % i)
     cur = requests.get(url_for_items, timeout=5)
     #cur = urllib.urlopen(url2, timeout=5)
@@ -54,8 +54,30 @@ for i in data:
     }
 
     json_list.append(json_file)
-    print(json_list)
+    #print(json_list)
 
+
+
+g = json.dumps(json_list)
+u = json.loads(g)
+
+
+bigger_noticed_date = []
+for i in u:
+    if i['item']['time'] >= date and i['item']['score'] >= score:
+        bigger_noticed_date.append(i)
+
+print(bigger_noticed_date)
+
+# x = json.loads(json_list)
+# f = csv.writer(open("test.csv", "wb+"))
+# f.writerow(["category", "item", "title", "url", "descendants"])
+#
+# for x in x:
+#     f.writerow([x["category"],
+#                 x["item"]["title"],
+#                 x["item"]["url"],
+#                 x["item"]["descendants"]])
 
 
 # with open("file.txt", "w") as output:
