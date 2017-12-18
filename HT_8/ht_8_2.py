747f71b69d895a29e468f81bb18c08b3663d8a8b
zender991 <zender991@gmail.com>
2017-12-18 21:39:20 +0200

    add second task

--- HT_8/ht_8_2.py
+++ HT_8/ht_8_2.py
@@ -1,13 +1,72 @@
 from bs4 import BeautifulSoup
 import requests
+import os
+import csv
+import xlsxwriter
+import json
+from time import sleep
 
-url = "http://quotes.toscrape.com/"
-page = requests.get(url)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+try:
+    directory = ("/python-cources/HT_8/reports/2")  # set path to reports directory
+    if not os.path.exists(directory):
+        os.makedirs(directory)  # create directory if it doesn't exist
+except Exception as e:
+    print(e)
 
-quotes = soup.select('div.quote')[0]
 
-quotes.select('span.text')
+def collect_domains():
+    initial_url = "https://www.expireddomains.net"
+    has_next = True
+    next_page_url = '/deleted-biz-domains'
+    while has_next:
+        sleep(1)
+        print(next_page_url)
+        result = []
+        url = initial_url + next_page_url
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        links = soup.select('a.namelinks')
+        for link in links:
+            result.append(link.text)
 
-print(quotes)
+        write_to_csv(result)
+        write_to_xls(result)
+        write_to_txt(result)
+        write_to_json(result)
+        try:
+            next_page_url = soup.select('a.next')[0].get('href')
+        except:
+            has_next = False
+
+
+def write_to_csv(result):
+    csv_file = open('reports/2/domains.csv', 'a')
+    csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+
+    for i in result:
+        csv_writer.writerow([i])
+
+
+def write_to_xls(result):
+    workbook = xlsxwriter.Workbook('reports/2/domains.xls')
+    worksheet = workbook.add_worksheet()
+
+    row = 0
+    col = 0
+    for i in result:
+        worksheet.write(row, col, i)
+        row += 1
+
+
+def write_to_txt(result):
+    with open('reports/2/domains.txt', 'a') as fp:
+        fp.write(str(result))
+
+
+def write_to_json(result):
+    with open('reports/2/domains.json', 'a') as fp:
+        json.dump(result, fp)
+
+
+collect_domains()
