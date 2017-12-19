682520c3edcdf6eb447aacd6fef5527cf25e2949
zender991 <zender991@gmail.com>
2017-12-19 22:12:13 +0200

    add comments

--- HT_8/ht_8_1.py
+++ HT_8/ht_8_1.py
@@ -17,146 +17,145 @@ except Exception as e:
 def find_full_info():
 
     next_page_url = ''
-    has_next = True
-    while has_next:
-        url = "http://quotes.toscrape.com" + next_page_url
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html5lib')
+    has_next = True     # value by deafult for loop start
+    while has_next:     # check flag
+        url = "http://quotes.toscrape.com" + next_page_url      # add to initial url part for the next page
+        page = requests.get(url)                                # get data from url
+        soup = BeautifulSoup(page.content, 'html5lib')          # transfer data to bs
         result = []
 
-        for a in soup.find_all("div", {"itemtype": "http://schema.org/CreativeWork"}):
-            quote = a.contents[1].text
+        for a in soup.find_all("div", {"itemtype": "http://schema.org/CreativeWork"}):  # find all blocks with info
+            quote = a.contents[1].text                          # store quote text
 
-            author_name = a.contents[3].find("small", {"itemprop":"author"}).text
-            author_url = a.contents[3].find("a").get("href")
-            author_id = author_url.split("/")[-1].lower()
-            author_full_url = url + author_url
+            author_name = a.contents[3].find("small", {"itemprop":"author"}).text   # store author name
+            author_url = a.contents[3].find("a").get("href")            # get href value from a tag
+            author_id = author_url.split("/")[-1].lower()               # create id for author
+            author_full_url = url + author_url                          # create full url for author page
 
-            author_details = requests.get(author_full_url)
-            author_soup = BeautifulSoup(author_details.content, 'html5lib')
+            author_details = requests.get(author_full_url)              # get info for external page
+            author_soup = BeautifulSoup(author_details.content, 'html5lib')     # transfer data to bs
 
-            for item in author_soup.find_all("div", {"class": "author-details"}):
-                author_born_date = item.contents[3].find("span", {"class": "author-born-date"}).text
+            for item in author_soup.find_all("div", {"class": "author-details"}):   # find all blovks with info
+                author_born_date = item.contents[3].find("span", {"class": "author-born-date"}).text    # store data
                 author_location = item.contents[3].find("span", {"class": "author-born-location"}).text
                 author_description = item.find("div", {"class": "author-description"}).text
 
-            dict = {"text": quote[1:-1]}
-            dict["author"] = {
+            dict = {"text": quote[1:-1]}    # write text without quotemarks into dictionary
+            dict["author"] = {              # write author details into dictionary
                 "url": author_full_url,
                 "author-title": author_name,
                 "born_date": author_born_date,
-                "born_place": author_location[3:],
-                "author_description": author_description[:100].lstrip(),
+                "born_place": author_location[3:],  # skip first 3 empty chars
+                "author_description": author_description[:100].lstrip(),  # write first 100 chars
                 "author_id": author_id
             }
 
             tags_list = []
-            for tag in a.contents[5].find_all("a", {"class": "tag"}):
-                tags_list.append({
+            for tag in a.contents[5].find_all("a", {"class": "tag"}):   # find all blocks with tag info
+                tags_list.append({                                      # write tag info into a dictionary
                     "tag_name": tag.text,
                     "tag_url": "http://quotes.toscrape.com" + tag.get("href")
                 })
 
             dict["tags"] = tags_list
 
-            result.append(dict)
-        write_to_csv_full_info(result)
+            result.append(dict)                         # collect all info and add to list
+        write_to_csv_full_info(result)                  # write to 4 files after parse each page
         write_to_json_file(result, "full_results")
         write_to_txt_file(result, "full_results")
         write_to_xls_full_info(result)
 
         try:
-            next_page_url = soup.select('ul.pager')[0].select('li.next a')[0].get('href')
+            next_page_url = soup.select('ul.pager')[0].select('li.next a')[0].get('href')   # find next page url
         except:
-            has_next = False
+            has_next = False                        # end loop if hasn't next page url
 
 
 def find_all_authors():
     next_page_url = ''
-    has_next = True
+    has_next = True         # value by deafult for loop start
     authors_list = []
-    while has_next:
-        url = "http://quotes.toscrape.com" + next_page_url
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html5lib')
+    while has_next:         # check flag
+        url = "http://quotes.toscrape.com" + next_page_url      # add to initial url part for the next page
+        page = requests.get(url)                                # get data from url
+        soup = BeautifulSoup(page.content, 'html5lib')          # transfer data to bs
 
 
-        for i in soup.find_all("div", {"itemtype": "http://schema.org/CreativeWork"}):
+        for i in soup.find_all("div", {"itemtype": "http://schema.org/CreativeWork"}):  # find all blocks with info
             result = []
-            author_name = i.contents[3].find("small", {"itemprop": "author"}).text
-            if author_name not in authors_list:
-                author_url = i.contents[3].find("a").get("href")
+            author_name = i.contents[3].find("small", {"itemprop": "author"}).text      # find author block
+            if author_name not in authors_list:                     # check if author already in a list
+                author_url = i.contents[3].find("a").get("href")    # if no, store info about this author
                 author_full_url = url + author_url
 
-                author_details = requests.get(author_full_url)
-                author_soup = BeautifulSoup(author_details.content, 'html5lib')
+                author_details = requests.get(author_full_url)      # get data from external page
+                author_soup = BeautifulSoup(author_details.content, 'html5lib')     # tranfer data to bs
 
-                for item in author_soup.find_all("div", {"class": "author-details"}):
+                for item in author_soup.find_all("div", {"class": "author-details"}):   # store author info
                     author_born_date = item.contents[3].find("span", {"class": "author-born-date"}).text
                     author_location = item.contents[3].find("span", {"class": "author-born-location"}).text
                     author_description = item.find("div", {"class": "author-description"}).text
 
                 dict = {}
-                dict["author"] = {
+                dict["author"] = {      # write author info into a dictionary
                     "url": author_full_url,
                     "author-title": author_name,
                     "born_date": author_born_date,
                     "born_place": author_location[3:],
                     "author_description": author_description[:100].lstrip()
                 }
-                authors_list.append(author_name)
-                result.append(dict)
-                write_to_json_file(result, "authors_details")
+                authors_list.append(author_name)    # add author to unique authors list
+                result.append(dict)                 # add dictionary to a result list
+                write_to_json_file(result, "authors_details")   # write to 4 files after parse each page
                 write_to_txt_file(result, "authors_details")
                 write_to_csv_all_authors(result)
                 write_to_xls_all_authors(result)
             else:
-                pass
+                pass                                            # skip  iteration if author already in a list
 
         try:
-            next_page_url = soup.select('ul.pager')[0].select('li.next a')[0].get('href')
+            next_page_url = soup.select('ul.pager')[0].select('li.next a')[0].get('href')   # find next page url
         except:
-            has_next = False
+            has_next = False                # finish loop if page hasn't next page url
 
 
 def find_all_tags():
     next_page_url = ''
-    has_next = True
+    has_next = True                          # value by deafult for loop start
     tags_list = []
 
-    while has_next:
-        url = "http://quotes.toscrape.com" + next_page_url
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html5lib')
+    while has_next:                          # check flag
+        url = "http://quotes.toscrape.com" + next_page_url      # add to initial url part for the next page
+        page = requests.get(url)                                # get data from url
+        soup = BeautifulSoup(page.content, 'html5lib')          # transfer data to bs
 
-        for item in soup.find_all("div", {"class": "tags"}):
-            tags = item.find_all("a", {"class": "tag"})
+        for item in soup.find_all("div", {"class": "tags"}):    # find all block with tags
+            tags = item.find_all("a", {"class": "tag"})         # find all tags inside each block
             for tag in tags:
                 if tag.text not in tags_list:
-                    tags_list.append(tag.text)
-
+                    tags_list.append(tag.text)                  # write unique tags in a list
                 else:
                     pass
 
             try:
                 next_page_url = soup.select('ul.pager')[0].select('li.next a')[0].get('href')
             except:
-                has_next = False
+                has_next = False                                # skip  iteration if author already in a list
 
-    write_to_json_file(tags_list, "all_tags")
+    write_to_json_file(tags_list, "all_tags")                   # write to 4 files after parse each page
     write_to_txt_file(tags_list, "all_tags")
     write_tags_to_xls(tags_list)
     write_tags_to_csv(tags_list)
 
 
-def find_author(*search_queries):
+def find_author(*search_queries):                               # func can obtain multiple value
    found_authors_list = []
-   for query in search_queries:
-      for i in read_from_json_file():
+   for query in search_queries:                                 # select each search query
+      for i in read_from_json_file():                           # read each line in json file
           for item in i:
-              current_author = item['author']['author_id']
-              if current_author == query:
-                  found_author = {
+              current_author = item['author']['author_id']      # find author in each record inside current item
+              if current_author == query:                       # compare found author with search query
+                  found_author = {                              # write data about searched author
                       "author_name": item['author']['author-title'],
                       "author_url": item['author']['url'],
                       "author_born_date": item['author']['born_date'],
@@ -164,16 +163,15 @@ def find_author(*search_queries):
                       "author_description": item['author']['author_description']
                   }
 
-
-          found_authors_list.append(found_author)
-          break
+          found_authors_list.append(found_author)               # add to list info about searched authors
+          break                                                 # don't need to continue loop if author found
 
    return found_authors_list
 
 
 def read_from_json_file():
     data = []
-    with open('reports/full_results.json') as f:
+    with open('reports/full_results.json') as f:                # read data from json file according to it format
         for line in f:
             data.append(json.loads(line))
 
@@ -182,13 +180,13 @@ def read_from_json_file():
 # ---- For full info ----------
 
 def write_to_csv_full_info(result):
-    csv_file = open('reports/1/full_results.csv', 'a')
-    csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+    csv_file = open('reports/1/full_results.csv', 'a')          # add ability to write to the file end
+    csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)    # initiate writer
 
     for i in result:
-        csv_writer.writerow("##")
+        csv_writer.writerow("##")                               # add separator for records
         quote_csv = "Quote", i["text"]
-        csv_writer.writerow(quote_csv)
+        csv_writer.writerow(quote_csv)                          # write each value to the columns. Below the same
         csv_writer.writerow(["Author"])
         author_url_csv = "", "Author url", i["author"]["url"]
         csv_writer.writerow(author_url_csv)
@@ -204,20 +202,20 @@ def write_to_csv_full_info(result):
         csv_writer.writerow(author_id_csv)
         tags_csv = "Tags", i["tags"]
         csv_writer.writerow(tags_csv)
-        csv_writer.writerow("")
+        csv_writer.writerow("")                                 # add empty string
 
 
 def write_to_xls_full_info(result):
     workbook = xlsxwriter.Workbook('reports/1/full_results.xls')
-    worksheet = workbook.add_worksheet()
+    worksheet = workbook.add_worksheet()                            # initiate worksheet
 
     row = 0
     col = 0
     for i in result:
-        worksheet.write(row, col, "##")
-        row += 1
+        worksheet.write(row, col, "##")                             # add separator for records
+        row += 1                                                    # increase row number
         worksheet.write(row, col, "Quote")
-        worksheet.write(row, col + 1, i["text"])
+        worksheet.write(row, col + 1, i["text"])                    # write each value to the columns. Below the same
         row += 1
         worksheet.write(row, col, "Author")
         row += 1
@@ -236,7 +234,7 @@ def write_to_xls_full_info(result):
         worksheet.write(row, col + 1, "Author description")
         worksheet.write(row, col + 2, i["author"]["author_description"])
         row += 1
-        worksheet.write(row, col, "")
+        worksheet.write(row, col, "")                               # add empty string
         row += 1
 
 # ---- End for full info ---------
@@ -244,12 +242,12 @@ def write_to_xls_full_info(result):
 # ----- For all authors ----------
 
 def write_to_csv_all_authors(result):
-    csv_file = open('reports/1/authors_details.csv', 'a')
-    csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+    csv_file = open('reports/1/authors_details.csv', 'a')       # add ability to write to the file end
+    csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)    # initiate writer
 
     for i in result:
-        csv_writer.writerow("##")
-        author_url_csv = "Author url", i["author"]["url"]
+        csv_writer.writerow("##")                               # add separator for records
+        author_url_csv = "Author url", i["author"]["url"]       # write each value to the columns. Below the same
         csv_writer.writerow(author_url_csv)
         author_title_csv = "Author name", i["author"]["author-title"]
         csv_writer.writerow(author_title_csv)
@@ -264,14 +262,14 @@ def write_to_csv_all_authors(result):
 
 def write_to_xls_all_authors(result):
     workbook = xlsxwriter.Workbook('reports/1/authors_details.xls')
-    worksheet = workbook.add_worksheet()
+    worksheet = workbook.add_worksheet()                                # initiate worksheet
 
     row = 0
     col = 0
     for i in result:
-        worksheet.write(row, col, "##")
-        row += 1
-        worksheet.write(row, col, "Author url")
+        worksheet.write(row, col, "##")                                 # add separator for records
+        row += 1                                                        # increase row number
+        worksheet.write(row, col, "Author url")                       # write each value to the columns. Below the same
         worksheet.write(row, col + 1, i["author"]["url"])
         row += 1
         worksheet.write(row, col, "Author name")
@@ -294,35 +292,36 @@ def write_to_xls_all_authors(result):
 # ------- For all tags -------------
 
 def write_tags_to_csv(result):
-    csv_file = open('reports/1/all_tags.csv', 'a')
-    csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+    csv_file = open('reports/1/all_tags.csv', 'a')              # add ability to write to the file end
+    csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)    # initiate writer
 
     for i in result:
-        csv_writer.writerow([i])
+        csv_writer.writerow([i])                                # write each tag to a file
 
 
 def write_tags_to_xls(result):
     workbook = xlsxwriter.Workbook('reports/1/all_tags.xls')
-    worksheet = workbook.add_worksheet()
+    worksheet = workbook.add_worksheet()                        # initiate worksheet
 
     row = 0
     col = 0
 
     for i in result:
-        worksheet.write(row, col, i)
-        row += 1
+        worksheet.write(row, col, i)                            # write each tag to a file
+        row += 1                                                # increase row number
 
 # ------- End for all tags ---------
 
 def write_to_json_file(result, file_name):
-    with open('reports/1/%s.json' %file_name, 'a') as fp:
-        json.dump(result, fp)
-        fp.write('\n')
+    with open('reports/1/%s.json' %file_name, 'a') as fp:   # add ability to write to the file end
+        json.dump(result, fp)                               # write object to a file
+        fp.write('\n')                                      # write next value from the new line
+
 
 def write_to_txt_file(result, file_name):
-    with open('reports/1/%s.txt' % file_name, 'a') as fp:
-        fp.write(str(result))
-        fp.write('\n')
+    with open('reports/1/%s.txt' % file_name, 'a') as fp:   # add ability to write to the file end
+        fp.write(str(result))                               # write object to a file
+        fp.write('\n')                                      # write next value from the new line
 
 
 
@@ -330,4 +329,4 @@ def write_to_txt_file(result, file_name):
 # find_all_authors()
 # find_all_tags()
 
-print(find_author( "j-k-rowling", "albert-einstein"))
+print(find_author("j-k-rowling", "albert-einstein"))
