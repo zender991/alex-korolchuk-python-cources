51e07b852f69f904135113098e6b41e5fab039e5
zender991 <zender991@gmail.com>
2018-01-13 20:53:38 +0200

    add common func

--- HT_10/config.py
+++ HT_10/config.py
@@ -1,2 +1,31 @@
-insert_askstory_query = "INSERT INTO askstories (by, descendants, story_id, kids, score, text, time, title, type) " \
-                        "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+# ----------- insert queries -----------
+
+insert_askstories_query = "INSERT INTO askstories (by, descendants, story_id, kids, score, text, time, title, type, url) " \
+                        "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+insert_newstories_query = "INSERT INTO newstories (by, descendants, story_id, kids, score, text, time, title, type, url)" \
+                          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+insert_jobstories_query = "INSERT INTO jobstories (by, descendants, story_id, kids, score, text, time, title, type, url)" \
+                          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+insert_showstories_query = "INSERT INTO showstories (by, descendants, story_id, kids, score, text, time, title, type, url)" \
+                          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+# ----------- update queries -----------
+
+update_askstories_query = "UPDATE askstories SET by = %s, descendants = %s, story_id = %s, kids = %s, score = %s, " \
+                          "text = %s, time = %s, title = %s, type = %s, url = %s " \
+                          "WHERE story_id = %s"
+
+update_newstories_query = "UPDATE newstories SET by = %s, descendants = %s, story_id = %s, kids = %s, score = %s, " \
+                          "text = %s, time = %s, title = %s, type = %s, url = %s " \
+                          "WHERE story_id = %s"
+
+update_jobstories_query = "UPDATE jobstories SET by = %s, descendants = %s, story_id = %s, kids = %s, score = %s, " \
+                          "text = %s, time = %s, title = %s, type = %s, url = %s " \
+                          "WHERE story_id = %s"
+
+update_showstories_query = "UPDATE showstories SET by = %s, descendants = %s, story_id = %s, kids = %s, score = %s, " \
+                          "text = %s, time = %s, title = %s, type = %s, url = %s " \
+                          "WHERE story_id = %s"
